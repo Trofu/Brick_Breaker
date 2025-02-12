d65f9a34f98669f1273c61c4ce5ce81632d0df6f
jespinosac <juaespcue@alu.edu.gva.es>
2025-02-12 18:18:52 +0100

    PA4

--- lib/src/components/play_area.dart
+++ lib/src/components/play_area.dart
@@ -1,16 +1,15 @@
 import 'dart:async';
-
+import 'package:flame/collisions.dart'; // Add this import
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-
 import '../brick_breaker.dart';
-
-class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
+class PlayArea extends RectangleComponent with
+    HasGameReference<BrickBreaker> {
   PlayArea()
       : super(
-          paint: Paint()..color = const Color(0xfff2e8cf),
-        );
-
+      paint: Paint()..color = const Color(0xfff2e8cf),
+      children: [RectangleHitbox()], // Add this arameter
+  );
   @override
   FutureOr<void> onLoad() async {
     super.onLoad();
