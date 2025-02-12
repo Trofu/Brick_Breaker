5a6f3458dec58ca2c54545ed61e062a3ee9c056e
jespinosac <juaespcue@alu.edu.gva.es>
2025-02-12 18:08:21 +0100

    PA2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'src/brick_breaker.dart';
 void main() {
-  final game = FlameGame();
+  final game = BrickBreaker(); // Modify this line
   runApp(GameWidget(game: game));
 }
