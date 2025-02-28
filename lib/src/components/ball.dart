ab028a0b52892c40e9099ea3e6a3289d941deb8e
jespinosac <juaespcue@alu.edu.gva.es>
2025-02-28 16:22:31 +0100

    Arreglos de game over cuando nextLvL

--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -14,13 +14,13 @@ class Ball extends CircleComponent
     required double radius,
     required this.difficultyModifier,
   }) : super(
-    radius: radius,
-    anchor: Anchor.center,
-    paint: Paint()
-      ..color = const Color(0xff1e6091)
-      ..style = PaintingStyle.fill,
-    children: [CircleHitbox()],
-  );
+          radius: radius,
+          anchor: Anchor.center,
+          paint: Paint()
+            ..color = const Color(0xff1e6091)
+            ..style = PaintingStyle.fill,
+          children: [CircleHitbox()],
+        );
 
   final Vector2 velocity;
   final double difficultyModifier;
@@ -29,11 +29,13 @@ class Ball extends CircleComponent
   @override
   void update(double dt) {
     super.update(dt);
-    if(game.playState!=PlayState.won && game.playState!=PlayState.nextLvL){
-      if((position.x<0 || position.x>gameWidth) || (position.y<0 || position.y>gameHeight)){
-        if(game.world.children.query<Ball>().length == 1){
+    if (game.playState != PlayState.won &&
+        game.playState != PlayState.nextLvL) {
+      if ((position.x < 0 || position.x > gameWidth) ||
+          (position.y < 0 || position.y > gameHeight)) {
+        if (game.world.children.query<Ball>().length == 1) {
           game.onGameOver(PlayState.ballOut);
-        }else{
+        } else {
           removeFromParent();
         }
       }
@@ -42,25 +44,30 @@ class Ball extends CircleComponent
   }
 
   @override
-  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollisionStart(intersectionPoints, other);
 
     if (other is PlayArea) {
-      if (intersectionPoints.first.y <= 0 || intersectionPoints.first.y >= game.height) {
+      if (intersectionPoints.first.y <= 0 ||
+          intersectionPoints.first.y >= game.height) {
         velocity.y = -velocity.y;
-      } else if (intersectionPoints.first.x <= 0 || intersectionPoints.first.x >= game.width) {
+      } else if (intersectionPoints.first.x <= 0 ||
+          intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       }
       if (intersectionPoints.first.y >= game.height) {
         if (game.world.children.query<Ball>().length == 1) {
-            game.onGameOver(PlayState.gameOver);
+          if (game.playState == PlayState.nextLvL) return;
+          game.onGameOver(PlayState.gameOver);
         } else {
           removeFromParent();
         }
       }
     } else if (other is Bat) {
       velocity.y = -velocity.y;
-      velocity.x += (position.x - other.position.x) / other.size.x * game.width * 0.3;
+      velocity.x +=
+          (position.x - other.position.x) / other.size.x * game.width * 0.3;
     } else if (other is Brick) {
       velocity.y = -velocity.y;
       velocity.setFrom(velocity * difficultyModifier);
